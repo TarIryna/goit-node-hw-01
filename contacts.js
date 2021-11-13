ce151c40975d844ae483ff2abd37b1495b429b5b
Tar Iryna <tar86irina@gmail.com>
2021-11-13 19:50:06 +0200

    fixed 2

--- contacts.js
+++ contacts.js
@@ -5,63 +5,52 @@ const shortid = require("shortid");
 const contactsPath = path.resolve("./db/contacts.json");
 
 async function listContacts() {
-  try {
-    const data = await fs.readFile(contactsPath, "utf8");
-    const contacts = JSON.parse(data);
-    console.log(contacts);
-    return contacts;
-  } catch (error) {
-    console.log(error);
+  const data = await fs.readFile(contactsPath, "utf8");
+  const contacts = JSON.parse(data);
+  if (!contacts || contacts.length === 0) {
+    console.log("Contacts list is empty");
+    return;
   }
+  return contacts;
 }
 
 async function getContactById(contactId) {
-  try {
-    const data = await fs.readFile(contactsPath, "utf8");
-    const contacts = JSON.parse(data);
-    const contactById = contacts.find(({ id }) => id === contactId);
-    console.log(contactById);
-    return contactById;
-  } catch (error) {
-    console.log(error);
+  const contacts = await listContacts();
+  const contactById = contacts.find(({ id }) => id === contactId);
+  if (!contactById) {
+    console.log("There is no such contact");
+    return;
   }
+  return contactById;
 }
 
 async function removeContact(contactId) {
-  try {
-    const data = await fs.readFile(contactsPath, "utf8");
-    const contacts = JSON.parse(data);
-    const newListOfContacts = contacts.filter(
-      (contact) => contact.id !== contactId
-    );
-    const content = JSON.stringify(newListOfContacts);
-    await fs.writeFile(contactsPath, content);
-    console.log(newListOfContacts);
-  } catch (error) {
-    console.log(error);
-  }
+  const contacts = await listContacts();
+  const newListOfContacts = contacts.filter(
+    (contact) => contact.id !== contactId
+  );
+  const content = JSON.stringify(newListOfContacts);
+  fs.writeFile(contactsPath, content);
+  return newListOfContacts;
 }
 
 async function addContact(name, email, phone) {
   if (name && email && phone) {
-    try {
-      const data = await fs.readFile(contactsPath, "utf8");
-      const contacts = JSON.parse(data);
-      const newContact = {
-        id: shortid.generate(),
-        name: name,
-        email: email,
-        phone: phone,
-      };
-      const newListOfContacts = [...contacts, newContact];
-      const content = JSON.stringify(newListOfContacts);
-      console.log(newListOfContacts);
-      await fs.writeFile(contactsPath, content);
-    } catch (error) {
-      console.log(error);
-    }
+    const contacts = await listContacts();
+    const newContact = {
+      id: shortid.generate(),
+      name: name,
+      email: email,
+      phone: phone,
+    };
+    const newListOfContacts = [...contacts, newContact];
+    const content = JSON.stringify(newListOfContacts);
+    console.log(newListOfContacts);
+    fs.writeFile(contactsPath, content);
+    return newListOfContacts;
   }
   console.log("All required fields are empty!");
+  return;
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const argv = require("yargs").argv;
 const contactsOperations = require("./contacts");
 
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      contactsOperations.listContacts();
-      break;
+async function invokeAction({ action, id, name, email, phone }) {
+  try {
+    switch (action) {
+      case "list":
+        const listOfContacts = await contactsOperations.listContacts();
+        console.log(listOfContacts);
+        break;
 
-    case "get":
-      contactsOperations.getContactById(id);
-      break;
+      case "get":
+        console.log(await contactsOperations.getContactById(id));
+        break;
 
-    case "add":
-      contactsOperations.addContact(name, email, phone);
-      break;
+      case "add":
+        console.log(await contactsOperations.addContact(name, email, phone));
+        break;
 
-    case "remove":
-      contactsOperations.removeContact(id);
-      break;
+      case "remove":
+        console.log(await contactsOperations.removeContact(id));
+        break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
